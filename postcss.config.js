abb52888a7e1fb82bd40fbc0c195d561e190bdb4
Acharya Group <acharyasumit786@gmail.com>
2025-09-04 15:55:32 +0530

    successful deploy frontend

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   plugins: [
+    require('postcss-flexbugs-fixes'),
     require('tailwindcss'),
     require('autoprefixer'),
-    require('cssnano')({
-      preset: 'default',
-    }),
+    require('cssnano')({ preset: 'default' }),
   ],
 }
